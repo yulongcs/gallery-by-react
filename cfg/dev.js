630b3a757a207bb9b677a9449fd470efb64d8d03
Endy Yu <yulongcs@hotmail.com>
2017-08-16 23:40:13 +0800

    learn webpack

--- cfg/dev.js
+++ cfg/dev.js
@@ -29,7 +29,7 @@ let config = Object.assign({}, baseConfig, {
 // Add needed loaders to the defaults here
 config.module.loaders.push({
   test: /\.(js|jsx)$/,
-  loader: 'react-hot!babel-loader',
+  loader: 'react-hot!babel-loader', //从右往左执行
   include: [].concat(
     config.additionalPaths,
     [ path.join(__dirname, '/../src') ]
